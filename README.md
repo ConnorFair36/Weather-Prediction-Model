6dec4f56b6a515f58e13089e073cce3cc20d5c12
ConnorFair36 <connorfair.36@gmail.com>
2026-02-11 11:36:23 -0500

    Created a PyTorch Dataloader for reading in time-series data

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Weather-Prediction-Model
+
+## Resources
+
+[A case study of spatiotemporal forecasting techniques for weather forecasting](https://arxiv.org/abs/2209.14782v2)
+
+[Convolutional LSTM Network: A Machine Learning Approach for Precipitation Nowcasting](https://arxiv.org/abs/1506.04214)
--- training/read_data.py
+++ training/read_data.py
@@ -1,132 +1,59 @@
-import marimo
+import xarray as xr
+import numpy as np
+import dask
+import zarr
+from torch.utils.data import Dataset
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
 
-__generated_with = "0.19.6"
-app = marimo.App(width="medium")
-
-
-@app.cell
-def _():
-    import marimo as mo
-    import xarray as xr
-    import numpy as np
-    import gribapi
-    import matplotlib.pyplot as plt
-    import pandas as pd
-    return mo, np, xr
+from sklearn.model_selection import train_test_split
 
 
-@app.cell
-def _(xr):
-    ds = xr.open_dataset("./f984e1115079d9c8f213746f9fc45def.grib", engine="cfgrib")
-    ds
-    return (ds,)
+class WeatherTrainingData(Dataset):
+    def __init__(self, dir: str, set_type: str, seq_length=6, transform=None, splits=(0.8, 0.1, 0.1), seed=42, n_months=12):
+        """Takes the path to the zarr dataset, sequence length and if you want the test, train or validation set as input."""
+        # yell at the user for being wrong
+        if set_type not in ["train", "test", "validation"]:
+            raise ValueError("set_type must be one of the following: [train, test, validation]")
+        self.weather_dataset = xr.open_dataset(dir, engine="zarr")
+        # sequence length should be divisible by the total length
+        self.seq_length = seq_length
+        self.transform = transform
+        # create the test, train and validation splits evenly
+        length = self.weather_dataset.sizes["valid_time"] // self.seq_length
+        all_indexes = list(range(length))
+        # aproximates the months to ensure even distribution of samples
+        month_aprx = [i % length//n_months for i in range(length)]
 
+        # split between training and test/validation
+        train_idx, other_idx, _, month_aprx = train_test_split(all_indexes, month_aprx, train_size=splits[0], stratify=month_aprx, random_state=seed)
+        # split between test and validation
+        test_idx, validation_idx = train_test_split(other_idx, train_size=splits[1]/(splits[1] + splits[2]), stratify=month_aprx, random_state=seed)
 
-@app.cell
-def _(ds):
-    print(ds.data_vars)
-    print(ds.coords)
-    return
+        if set_type == "test":
+            self.indexes = test_idx
+        elif set_type == "validation":
+            self.indexes = validation_idx
+        else:
+            self.indexes = train_idx
 
+    def __len__(self):
+        """Returns the length of the dataset - the sequence length, which is the number of groups of batches stored."""
+        return len(self.indexes)
 
-@app.cell
-def _(ds):
-    ds["step"]
-    return
+    def __getitem__(self, idx):
+        """Gets the data stored for a particular hour with the dimentions (latitude, longitude, time step, weather variables)."""
+        # translate the inputed index to the index for the dataset
+        idx = self.indexes[idx]
+        # gets the subset of the dataset we are looking for
 
-
-@app.cell
-def _(ds):
-    ds.isel(time=0, step=23)
-    return
-
-
-@app.cell
-def _(ds, np):
-    ds_1h = ds.sel(step=np.timedelta64(1, "h"))
-    ds_1h["t2m"].isel(time=1).plot()
-
-    return
-
-
-@app.cell
-def _(ds):
-    tp = ds["tp"]
-    return (tp,)
-
-
-@app.cell
-def _(tp):
-    print(tp.shape)
-    print(tp.dims)
-    return
-
-
-@app.cell
-def _(tp):
-    tp.isel(time=1, step=0)
-    return
-
-
-@app.cell
-def _(ds):
-    # create valid time
-    ds_stacked = ds.assign_coords(
-        valid_time=ds["time"] + ds["step"]
-    )
-    # compress time & step into 1 dimention
-    ds_stacked = ds_stacked.stack(t=("time", "step"))
-    # remove all entries where all values are nan
-    ds_stacked = ds_stacked.dropna(dim="t", how="all")
-    # remove unessicary columns?
-    ds_clean = (
-        ds_stacked
-        .set_index(t="valid_time")
-        .rename({"t": "valid_time"})
-        .drop_vars(["time", "step"])
-        .sortby("valid_time")
-    )
-    # profit
-    ds_clean.isel(valid_time=0)
-    return (ds_clean,)
-
-
-@app.cell
-def _(ds_clean):
-    ds_clean["t2m"].mean(dim=["latitude", "longitude"]).plot(marker="o")
-
-    return
-
-
-@app.cell
-def _(ds_clean):
-    ds_sampled = ds_clean.coarsen(latitude=4, boundary='pad').mean().coarsen(longitude=4, boundary='pad').mean()
-    return (ds_sampled,)
-
-
-@app.cell
-def _(mo):
-    slider = mo.ui.slider(start=0, stop=23, label="Slider", value=3)
-    return (slider,)
-
-
-@app.cell
-def _(mo, slider):
-    mo.hstack([slider, mo.md(f"Has value: {slider.value}")])
-    return
-
-
-@app.cell
-def _(ds_sampled, slider):
-    ds_sampled["tp"].isel(valid_time=slider.value).plot()
-    return
-
-
-@app.cell
-def _(ds_sampled):
-    ds_sampled
-    return
-
-
-if __name__ == "__main__":
-    app.run()
+        sample_dims = ['latitude', 'longitude', 'valid_time']
+        # convert the substep from dataset to -> stacked array -> numpy -> torch tensor
+        stacked_array = self.weather_dataset.isel(valid_time=range(idx * self.seq_length, (idx+1) * self.seq_length)).to_stacked_array("time", sample_dims=sample_dims)
+        numpy_array = stacked_array.to_numpy()
+        sample = torch.from_numpy(numpy_array)
+        # do a transformation of the final tensor if applicable
+        if self.transform:
+            sample = self.transform(sample)
+        return sample
